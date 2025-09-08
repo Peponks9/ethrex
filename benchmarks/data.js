956c4437b1dcf6d4ce8677ad8715856beca33b01
github-action-benchmark <github@users.noreply.github.com>
2025-09-08 18:01:57 +0000

    add Benchmark (cargo) benchmark result for f88b87a81aea2d96ea942ad1c7fb72e04bc3c1e7

---
+++ benchmarks/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1757354517756,
+  "repoUrl": "https://github.com/Peponks9/ethrex",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "52646071+Peponks9@users.noreply.github.com",
+            "name": "josé v",
+            "username": "Peponks9"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f88b87a81aea2d96ea942ad1c7fb72e04bc3c1e7",
+          "message": "Merge branch 'lambdaclass:main' into main",
+          "timestamp": "2025-09-08T11:18:37-06:00",
+          "tree_id": "7828b60b5cb4512eda1d4f94110580ed1da19845",
+          "url": "https://github.com/Peponks9/ethrex/commit/f88b87a81aea2d96ea942ad1c7fb72e04bc3c1e7"
+        },
+        "date": 1757354516083,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 166468647748,
+            "range": "± 329815295",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
